b5067ea5dfbd19dbcd7ace2149919a5bf5421698
Dean Itani <muhiedin.itani@gmail.com>
2021-12-12 15:31:57 -0600

    add search page

---
+++ src/views/Search.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="search">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to the Search Page",
+      };
+    },
+    created: function () {},
+    methods: {},
+  };
+</script>
