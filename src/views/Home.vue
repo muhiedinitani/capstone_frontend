f5f314c9ec80c1318c21bb36d47e56ef292a2323
Dean Itani <muhiedin.itani@gmail.com>
2021-12-12 15:19:08 -0600

    home page is resorts index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,37 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="resort in resorts" v-bind:key="resort.id">
+    <div> {{resort.name}} </div>
+    <div> {{resort.official_website}} </div>
+    <div> {{resort.lat}} </div>
+    <div> {{resort.lng}} </div>
+    <hr>
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
   export default {
     data: function () {
       return {
-        message: "Welcome to Vue.js!",
+        message: "Welcome to Resorts Index",
+        resorts: []
       };
     },
-    created: function () {},
-    methods: {},
+    created: function () {
+      this.indexResorts();
+    },
+    methods: {
+      indexResorts: function() {
+        axios.get("/resorts").then((response) => {
+          console.log("you are in resorts index", response);
+          this.resorts = response.data
+        })
+      }
+    },
   };
 </script>
