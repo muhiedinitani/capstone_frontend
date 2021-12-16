f013f9c6a20837359c94537a0305a3a21cad9d81
Dean Itani <muhiedin.itani@gmail.com>
2021-12-15 19:20:08 -0600

    popper install

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import axios from "axios";
+import axios from 'axios'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+Vue.use(BootstrapVue)
+import Popper from 'popper.js'
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
